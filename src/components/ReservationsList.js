47edfbccdb399792426db6acdccdd3827c2e1a68
umair-rao <umairmuqeem@gmail.com>
2024-02-27 23:58:14 +0500

    Display Reservation List Items Details

--- src/components/ReservationsList.js
+++ src/components/ReservationsList.js
@@ -1,12 +1,19 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import Carousel from 'react-elastic-carousel';
-import { fetchReservations, selectAllReservations, deleteReservation } from '../redux/reservationsSlice';
+import {
+  fetchReservations,
+  selectAllReservations,
+  deleteReservation,
+} from '../redux/reservationsSlice';
 import style from '../styles/Vehicles.module.css';
 
 function ReservationsList() {
   const dispatch = useDispatch();
   const reservations = useSelector(selectAllReservations);
+  const itemData = useSelector((state) => state.item.item);
+  const isLoading = useSelector((state) => state.reservations.loading);
 
   useEffect(() => {
     dispatch(fetchReservations());
@@ -16,38 +23,63 @@ function ReservationsList() {
     dispatch(deleteReservation(id));
   };
 
-  const renderReservations = () => (
-    reservations.map((reservation) => (
-      <li key={reservation.id} className={style['vehicle-card']}>
-        <div className={style.img}>
-          <img
-            src={reservation.image}
-            alt={reservation.name}
-          />
-        </div>
-        <p>
-          Name :
-          {reservation.name}
-        </p>
-        <p>
-          Description :
-          {reservation.description}
-        </p>
-        <p>
-          Reserved for date :
-          {reservation.reserve_for_use_date}
-        </p>
-        <p className={style['prev-reserve']}>
-          You Reserved This Car From:
-          {' '}
-          <span>
-            {reservation.city}
-          </span>
-        </p>
-        <button type="button" className={style['delete-button']} onClick={() => handleDeleteReservation(reservation.id)}>Delete Reservation</button>
-      </li>
-    ))
-  );
+  const filterReservations = () => reservations.filter((reservationItem) => {
+    const matchingDataItem = itemData?.items?.find(
+      (dataItem) => dataItem.id === reservationItem.item_id,
+    );
+    return matchingDataItem !== undefined;
+  });
+  const filteredReservations = filterReservations();
+
+  const renderReservations = () => filteredReservations.map((reservation) => (
+    <li key={reservation.id} className={style['vehicle-card']}>
+      <div className={style.img}>
+        <img
+          src={
+              itemData?.items?.find(
+                (dataItem) => dataItem.id === reservation.item_id,
+              )?.image
+            }
+          alt={reservation.name}
+        />
+      </div>
+      <p>
+        Name :
+        {' '}
+        {
+            itemData?.items?.find(
+              (dataItem) => dataItem.id === reservation.item_id,
+            )?.name
+          }
+      </p>
+      <p>
+        Description :
+        {' '}
+        {
+            itemData?.items?.find(
+              (dataItem) => dataItem.id === reservation.item_id,
+            )?.description
+          }
+      </p>
+      <p>
+        Reserved for date :
+        {' '}
+        {reservation.reserveForUseDate}
+      </p>
+      <p className={style['prev-reserve']}>
+        You Reserved This Car From:
+        {' '}
+        <span>{reservation.city}</span>
+      </p>
+      <button
+        type="button"
+        className={style['delete-button']}
+        onClick={() => handleDeleteReservation(reservation.id)}
+      >
+        Delete Reservation
+      </button>
+    </li>
+  ));
 
   const breakPoints = [
     { width: 1, itemsToShow: 1 },
@@ -59,9 +91,11 @@ function ReservationsList() {
   return (
     <div>
       <h2 className={style['vehicle-heading']}>Your Reservations</h2>
-      <Carousel breakPoints={breakPoints}>
-        {renderReservations()}
-      </Carousel>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <Carousel breakPoints={breakPoints}>{renderReservations()}</Carousel>
+      )}
     </div>
   );
 }
